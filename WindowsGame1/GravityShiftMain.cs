2e6cbb38092ce76964b453b5b01bc5756d8d7bd7
Tyler Robinson <tydiz936@gmail.com>
2010-10-06 15:27:11 -0600

    Removal of Nate's commit test

--- WindowsGame1/GravityShiftMain.cs
+++ WindowsGame1/GravityShiftMain.cs
@@ -11,7 +11,6 @@ using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
 
-/*A NOOB MADE THIS CODE*/
 namespace GravityShift
 {
     /// <summary>
