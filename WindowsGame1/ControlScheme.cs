c0cc090d53f9d9e52d30caf9270a0b24afbc57ae
unknown <Curtis@.(none)>
2010-10-29 16:11:33 -0600

    Fix error where it would call isPressed()  multiple times

--- WindowsGame1/ControlScheme.cs
+++ WindowsGame1/ControlScheme.cs
@@ -19,24 +19,28 @@ namespace GravityShift
     {
         /// <summary>
         /// Checks to see if the control for left has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         bool isLeftPressed(GameTime gametime);
 
         /// <summary>
         /// Checks to see if the control for Right has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         bool isRightPressed(GameTime gametime);
 
         /// <summary>
         /// Checks to see if the control for Down has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         bool isDownPressed(GameTime gametime);
 
         /// <summary>
         /// Checks to see if the control for Up has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         bool isUpPressed(GameTime gametime);
@@ -55,7 +59,14 @@ namespace GravityShift
     {
         private const double BURN_DOWN_TIME = 1000;
         GameTime mGametime = new GameTime(System.TimeSpan.Zero, System.TimeSpan.Zero, System.TimeSpan.Zero, System.TimeSpan.Zero);
-
+        bool upIsPushed = false;
+        bool downIsPushed = false;
+        bool leftIsPushed = false;
+        bool rightIsPushed = false;
+        bool upWasPushed = false;
+        bool downWasPushed = false;
+        bool leftWasPushed = false;
+        bool rightWasPushed = false;
         /// <summary>
         /// Checks to see if the given key is pressed
         /// </summary>
@@ -69,38 +80,97 @@ namespace GravityShift
 
         /// <summary>
         /// Checks to see if the control for left has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isLeftPressed(GameTime gametime)
         {
-            return isPressed(Keys.Left, gametime);
+            if (isPressed(Keys.Left, gametime))
+            {
+                leftIsPushed = true;
+            }
+            else
+            {
+                leftIsPushed = false;
+            }
+
+            if (leftIsPushed && !leftWasPushed)
+            {
+                return true;
+            }
+            leftWasPushed = leftIsPushed;
+            return false;
         }
 
         /// <summary>
         /// Checks to see if the control for Right has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isRightPressed(GameTime gametime)
         {
-            return isPressed(Keys.Right, gametime);
+            if (isPressed(Keys.Right, gametime))
+            {
+                rightIsPushed = true;
+            }
+            else
+            {
+                rightIsPushed = false;
+            }
+
+            if (rightIsPushed && !rightWasPushed)
+            {
+                return true;
+            }
+            rightWasPushed = rightIsPushed;
+            return false;
         }
 
         /// <summary>
         /// Checks to see if the control for Down has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isDownPressed(GameTime gametime)
         {
-            return isPressed(Keys.Down, gametime);
+            if (isPressed(Keys.Down, gametime))
+            {
+                downIsPushed = true;
+            }
+            else
+            {
+                downIsPushed = false;
+            }
+
+            if (downIsPushed && !downWasPushed)
+            {
+                return true;
+            }
+            downWasPushed = downIsPushed;
+            return false;
         }
 
         /// <summary>
         /// Checks to see if the control for Up has been interacted with
+        /// Only work when the button has just been pushed
         /// </summary>
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isUpPressed(GameTime gametime)
         {
-            return isPressed(Keys.Up, gametime);
+            if (isPressed(Keys.Up, gametime))
+            {
+                upIsPushed = true;
+            }
+            else
+            {
+                upIsPushed = false;
+            }
+            if (upIsPushed && !upWasPushed)
+            {
+                return true;
+            }
+            upWasPushed = upIsPushed;
+            return false;
         }
 
         
@@ -118,6 +188,15 @@ namespace GravityShift
     class ControllerControl : IControlScheme
     {
         private PlayerIndex mPlayerIndex;
+
+        bool upIsPushed = false;
+        bool downIsPushed = false;
+        bool leftIsPushed = false;
+        bool rightIsPushed = false;
+        bool upWasPushed = false;
+        bool downWasPushed = false;
+        bool leftWasPushed = false;
+        bool rightWasPushed = false;
 
         /// <summary>
         /// Constructor - Constructs a scheme for the controller input type
@@ -145,7 +224,20 @@ namespace GravityShift
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isLeftPressed(GameTime gametime)
         {
-            return isPressed(Buttons.DPadLeft) || isPressed(Buttons.LeftThumbstickLeft) || isPressed(Buttons.X);
+            if (isPressed(Buttons.DPadLeft) || isPressed(Buttons.LeftThumbstickLeft) || isPressed(Buttons.X))
+            {
+                leftIsPushed = true;
+            }
+            else
+            {
+                leftIsPushed = false;
+            }
+            if (leftIsPushed && !leftWasPushed)
+            {
+                return true;
+            }
+            leftWasPushed = leftIsPushed;
+            return false;
         }
 
         /// <summary>
@@ -154,7 +246,21 @@ namespace GravityShift
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isRightPressed(GameTime gametime)
         {
-            return isPressed(Buttons.DPadRight) || isPressed(Buttons.LeftThumbstickRight) || isPressed(Buttons.B);
+            if (isPressed(Buttons.DPadRight) || isPressed(Buttons.LeftThumbstickRight) || isPressed(Buttons.B))
+            {
+                rightIsPushed = true;
+            }
+            else
+            {
+                rightIsPushed = false;
+            }
+
+            if (rightIsPushed && !rightWasPushed)
+            {
+                return true;
+            }
+            rightWasPushed = rightIsPushed;
+            return false;
         }
 
         /// <summary>
@@ -163,7 +269,21 @@ namespace GravityShift
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isDownPressed(GameTime gametime)
         {
-            return isPressed(Buttons.DPadDown) || isPressed(Buttons.LeftThumbstickDown) || isPressed(Buttons.A);
+            if (isPressed(Buttons.DPadDown) || isPressed(Buttons.LeftThumbstickDown) || isPressed(Buttons.A))
+            {
+                downIsPushed = true;
+            }
+            else
+            {
+                downIsPushed = false;
+            }
+
+            if (downIsPushed && !downWasPushed)
+            {
+                return true;
+            }
+            downWasPushed = downIsPushed;
+            return false;
         }
 
         /// <summary>
@@ -172,7 +292,20 @@ namespace GravityShift
         /// <returns>True if it has been pressed; false otherwise</returns>
         public bool isUpPressed(GameTime gametime)
         {
-            return isPressed(Buttons.DPadDown) || isPressed(Buttons.LeftThumbstickDown) || isPressed(Buttons.Y);
+            if (isPressed(Buttons.DPadDown) || isPressed(Buttons.LeftThumbstickDown) || isPressed(Buttons.Y))
+            {
+                upIsPushed = true;
+            }
+            else
+            {
+                upIsPushed = false;
+            }
+            if (upIsPushed && !upWasPushed)
+            {
+                return true;
+            }
+            upWasPushed = upIsPushed;
+            return false;
         }
 
         /// <summary>
