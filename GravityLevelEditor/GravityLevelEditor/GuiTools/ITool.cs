1fb10749071c09223a0a03c2f82ef482eb159e21
Tyler Robinson <tydiz936@gmail.com>
2010-11-02 22:07:07 -0600

    Add tool classes into commit(oops, from last commit)

---
+++ GravityLevelEditor/GravityLevelEditor/GuiTools/ITool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GravityLevelEditor.GuiTools
+{
+    interface ITool
+    {
+        void LeftMouseDown(Level level, Point gridPosition);
+        void LeftMouseUp(Level level, Point gridPosition);
+        void RightMouseDown(Level level, Point gridPosition);
+        void RightMouseUp(Level level, Point gridPosition);
+        void MouseMove(Level level, Point gridPosition);
+    }
+}
