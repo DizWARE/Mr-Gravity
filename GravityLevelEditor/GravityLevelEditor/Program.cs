3f6eda337cd3ebfe73256ee8ef0dd23080d7e7f9
Kamron Egan <k.egan@utah.edu>
2010-11-18 16:33:18 -0700

    Add XmlKeys to Level Editor for saving/loading cleaness...

--- GravityLevelEditor/GravityLevelEditor/Program.cs
+++ GravityLevelEditor/GravityLevelEditor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using GravityLevelEditor.EntityCreationForm;
+using GravityLevelEditor.XML;
 
 namespace GravityLevelEditor
 {
