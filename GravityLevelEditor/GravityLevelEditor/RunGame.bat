daea019c3db5e12d1d0cc09e27fbf5cdb8fe535a
Tyler Robinson <tydiz936@gmail.com>
2010-11-23 15:40:02 -0700

    Edit game launch

--- GravityLevelEditor/GravityLevelEditor/RunGame.bat
+++ GravityLevelEditor/GravityLevelEditor/RunGame.bat
@@ -1,2 +1,11 @@
-cd ..\..\..\..\WindowsGame1\bin\x86\Debug\
-GravityShift.exe %1
+cd ..\..\..\..\WindowsGame1\bin\x86\Release\
+IF EXISTS GravityShift.exe
+( 
+	GravityShift.exe %1
+)
+IF NOT EXISTS GravityShift.exe
+(
+	cd ..\Debug\
+	GravityShift.exe %1
+)
+
