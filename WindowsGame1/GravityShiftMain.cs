129bc654e130c4ae8922111f37a2d12e0f5550f0
Kamron Egan <k.egan@utah.edu>
2011-02-18 19:47:08 -0700

    Consolidate menu code into a single folder for sanity's sake, more TitleSafe fixes, add controller code back into options

--- WindowsGame1/GravityShiftMain.cs
+++ WindowsGame1/GravityShiftMain.cs
@@ -49,6 +49,9 @@ namespace GravityShift
 
         Options mOptions;
         Credits mCredits;
+
+        //Instance of the Controller class
+        Controller mController;
 		
         private GameStates mCurrentState = GameStates.Title;
 
@@ -124,6 +127,8 @@ namespace GravityShift
             mOptions = new Options(mControls, mGraphics);
             mAfterScore = new AfterScore(mControls);
 
+            mController = new Controller(mControls, mGraphics);
+
             mSpriteBatch = new SpriteBatch(mGraphics.GraphicsDevice);
             base.Initialize();
 
@@ -165,6 +170,7 @@ namespace GravityShift
             mCurrentLevel = new Level(mLevelLocation, mControls, GraphicsDevice.Viewport);
             mCurrentLevel.Load(Content);
             mAfterScore.Load(Content, GraphicsDevice);
+            mController.Load(Content);
 
             // Create a new SpriteBatch, which can be used to draw textures.
             mSpriteBatch = new SpriteBatch(GraphicsDevice);
@@ -302,6 +308,10 @@ namespace GravityShift
             {
                 mPause.Update(gameTime, ref mCurrentState, ref mCurrentLevel);
             }
+            else if (mCurrentState == GameStates.Controls)
+            {
+                mController.Update(gameTime, ref mCurrentState);
+            }
             else if (mCurrentState == GameStates.Unlock)
             {
                 mLevelSelect.UnlockNextLevel();
@@ -436,6 +446,8 @@ namespace GravityShift
                 mOptions.Draw(gameTime, mSpriteBatch, scale);
                 mMainMenuLevel.Draw(mSpriteBatch, gameTime, scale);
             }
+            else if (mCurrentState == GameStates.Controls)
+                mController.Draw(gameTime, mSpriteBatch, scale);
             else if (mCurrentState == GameStates.Score)
                 mScoring.Draw(mSpriteBatch, mGraphics, scale);
             else if (mCurrentState == GameStates.Level_Selection)
