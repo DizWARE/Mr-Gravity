6b631ddf38301927011537f9d2aa8144affe451c
Tyler Robinson <tydiz936@gmail.com>
2011-04-15 12:32:49 -0600

    Fix level 9 bug

--- WindowsGame1/Menu Code/Credits.cs
+++ WindowsGame1/Menu Code/Credits.cs
@@ -47,7 +47,7 @@ namespace GravityShift
 
             //Easily add names and titles here
             mTitles = new Dictionary<string, string[]>();
-            mTitles.Add("Angry Newton", new string[] { "Production" });
+            mTitles.Add("Angry Newton Production", new string[] { "", "Angry" });
             mTitles.Add("Developed At", new string[] { "University Of Utah; Senior EAE Capstone Class" });
             mTitles.Add("Executive Producer", new string[]{"Roger Altizer", "Dr. Bob Kessler"});
             mTitles.Add("Original Concept & Design", new string[] { "Tyler Robinson" });
@@ -104,7 +104,7 @@ namespace GravityShift
         {
             if (mControls.isAPressed(false) || mControls.isStartPressed(false) || mControls.isBackPressed(false) || mControls.isBPressed(false))
             { mTopY = mGraphics.GraphicsDevice.Viewport.TitleSafeArea.Bottom; states = GameStates.Main_Menu; }
-            mTopY -= 1.5f;
+            mTopY -= 1.25f;
 
             mBackgroundLevel.Update(gametime, ref states);
         }
@@ -149,7 +149,7 @@ namespace GravityShift
                 spriteBatch.DrawString(mFontBig, key, 
                     Vector2.Add(new Vector2(GetTextXLocation(key, true), top), new Vector2(2,2)), Color.SteelBlue);
 
-
+                //If the first item under this key is empty, then this is an image block. 
                 if (mTitles[key][0] == "")
                 {
                     spriteBatch.Draw(PlayerFaces.FromString(mTitles[key][1]), new Rectangle(GetTextXLocation(key, true) - 3*mSad.Width/2, top, mSad.Width, mSad.Height), Color.White);
